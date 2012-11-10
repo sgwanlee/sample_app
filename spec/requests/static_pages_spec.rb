e013061e82cd41b5582a597c9b42756fa884c3e1
Seonggwan Lee <seonggwan.lee@gmail.com>
2012-11-11 02:17:15 +0900

    Finish Exercies 2 of 3.5

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -12,6 +12,8 @@ require 'spec_helper'
 
 
 describe "StaticPages" do
+
+	let(:base_title) { "Ruby on Rails Tutorial Sample App"}
 	describe "Home page" do
 
 		it "should have the content 'Sample App'" do
@@ -22,7 +24,7 @@ describe "StaticPages" do
 		it "should have the right title" do
 			visit '/static_pages/home'
 			page.should have_selector('title', 
-				text: " | Home")
+				text: "#{base_title} | Home")
 		end
 	end
 
@@ -37,7 +39,7 @@ describe "StaticPages" do
 		it "should have the right title" do
 			visit '/static_pages/help'
 			page.should have_selector('title', 
-				text: " | Help")
+				text: "#{base_title} | Help")
 		end
 	end	
 
@@ -52,7 +54,7 @@ describe "StaticPages" do
 		it "should have the right title" do
 			visit '/static_pages/about'
 			page.should have_selector('title', 
-				text: " | About Us")
+				text: "#{base_title} | About Us")
 		end
 	end
 
@@ -67,7 +69,7 @@ describe "StaticPages" do
 		it "should have the right title" do
 			visit '/static_pages/contact'
 			page.should have_selector('title', 
-				text: " | Contact")
+				text: "#{base_title} | Contact")
 		end
 	end
 
